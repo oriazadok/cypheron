f77e73a859de5a9c49b5980ff314f3579392fd84
oriazadok <oriazadok@gmail.com>
2024-08-15 15:26:10 +0300

    add a few basic screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'cypher_ffi.dart';
+import 'package:cypheron/ui/screens/home.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,53 +9,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter FFI Example',
+      title: 'Cypheron',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String cypherResult = '';
-  final CypherFFI cypherFFI = CypherFFI();
-
-  void _runCypher() {
-    setState(() {
-      cypherResult = cypherFFI.runCypher('Hello, World!', 'mySecretKey', 'e'); // 'e' for encrypt
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter FFI Example'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Cypher Result:'),
-            Text(
-              cypherResult,
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _runCypher,
-              child: Text('Run Cypher'),
-            ),
-          ],
-        ),
-      ),
+      home: Home(),
     );
   }
 }
